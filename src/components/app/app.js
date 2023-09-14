e81afccb84fca8542c854492c99c60be15c4ee54
Viktor <viktorF@gmail.net>
2023-09-14 16:55:27 +0300

    json-server

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,44 @@
-import './app.css'
+import Header from '../header/header'
+import Main from '../main/main'
+import React, { useEffect, useReducer } from 'react'
+
+const initState = {
+  question: []
+}
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'load':
+      return [...action.payload]
+    default:
+      return state
+  }
+}
 
 function App() {
-  return <div className='App'>555</div>
+  const [state, dispatch] = useReducer(reducer)
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      const res = await fetch(`http://localhost:8000/questions`)
+      const data = await res.json()
+      dispatch({ type: 'load', payload: data })
+    }
+
+    getQuestions()
+  }, [])
+
+  console.log(state)
+
+  return (
+    <div className='App'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/header/header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => {
+  return <h1 className={styles.header}>The react quiz</h1>
+}
+
+export default Header
